a61b6de9ac1bdc66fd91acaace251ea64f703922
Iago Colodetti <iagocolodetti1@gmail.com>
2019-01-27 00:50:09 -0200

    util.java deletado

--- src/java/br/com/iagocolodetti/controle/util.java
+++
@@ -1,9 +0,0 @@
-package br.com.iagocolodetti.controle;
-
-import java.nio.charset.StandardCharsets;
-
-public class util {
-    static public String decodificar(String codificado) {
-        return (new String(codificado.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8));
-    }
-}
